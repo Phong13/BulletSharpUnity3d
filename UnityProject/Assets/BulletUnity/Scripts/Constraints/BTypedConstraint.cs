e6d0afbf8bebfba34c3a2531c86a8e9249d7c983
Robert Culhane <rculhane@rathergoodengineering.com>
2015-12-27 23:57:12 -0500

    hinge constraint mods

--- UnityProject/Assets/BulletUnity/Scripts/Constraints/BTypedConstraint.cs
+++ UnityProject/Assets/BulletUnity/Scripts/Constraints/BTypedConstraint.cs
@@ -3,25 +3,47 @@ using UnityEngine;
 using System.Collections;
 using BulletSharp;
 
-namespace BulletUnity {
+namespace BulletUnity
+{
     [System.Serializable]
-    public abstract class BTypedConstraint : MonoBehaviour, IDisposable {
+    public abstract class BTypedConstraint : MonoBehaviour, IDisposable
+    {
+
+        protected RigidBody rigidBodyA;
+        public RigidBody RigidBodyA  //always on this object
+        {
+            get { return rigidBodyA = rigidBodyA ?? GetComponent<BRigidBody>().GetRigidBody(); }
+        }
+
+        //Assign in inspector
+        public BRigidBody BRigidBodB;
+        private RigidBody rigidBodyB;
+        public RigidBody RigidBodyB
+        {
+            get { return rigidBodyB = rigidBodyB ?? GetComponent<BRigidBody>().GetRigidBody(); }
+
+        }
+
 
         protected TypedConstraint constraintPtr = null;
         internal bool isInWorld;
 
-        void OnDestroy() {
+        void OnDestroy()
+        {
             Dispose(false);
         }
 
         //do not override this
-        public void Dispose() {
+        public void Dispose()
+        {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
 
-        protected virtual void Dispose(bool isdisposing) {
-            if (constraintPtr != null) {
+        protected virtual void Dispose(bool isdisposing)
+        {
+            if (constraintPtr != null)
+            {
                 constraintPtr.Dispose();
                 constraintPtr = null;
             }
