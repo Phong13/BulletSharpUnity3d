97cc9a66049bd5b7382537b6e25bb4fb2e68b573
Jai <neo5icekcore@gmail.com>
2016-02-25 14:55:26 +0100

    Added Icons for rigidbody and box scripts (they are atm low res), added a folder called InternalResources for manage all the things related to the editor so it won't get into the final build, added capsule custom editor and added the warning for the local scale

---
+++ UnityProject/Assets/BulletUnity/Scripts/Editor/BCapsuleShapeEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using BulletUnity;
+
+[CustomEditor(typeof(BCapsuleShape))]
+public class BCapsuleShapeEditor : Editor {
+
+	BCapsuleShape script;
+	SerializedProperty radius;
+	SerializedProperty height;
+
+	void OnEnable() {
+		script = (BCapsuleShape)target;
+		GetSerializedProperties();
+	}
+
+	void GetSerializedProperties() {
+		radius = serializedObject.FindProperty("radius");
+		height = serializedObject.FindProperty("height");
+	}
+
+	public override void OnInspectorGUI() {
+		if(script.transform.localScale != Vector3.one) {
+			EditorGUILayout.HelpBox("This shape doesn't support scale of the object.\nThe scale must be one", MessageType.Warning);
+		}
+		EditorGUILayout.PropertyField(radius);
+		EditorGUILayout.PropertyField(height);
+		serializedObject.ApplyModifiedProperties();
+	}
+}
