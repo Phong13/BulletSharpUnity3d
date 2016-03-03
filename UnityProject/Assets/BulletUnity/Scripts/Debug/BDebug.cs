d516a6f1bb5b2f4dbc8e18c69cd9fb9a07818bae
Jai <neo5icekcore@gmail.com>
2016-03-03 23:02:20 +0100

    Added new icon for cone shape, started debug mode and added it to a namespace

---
+++ UnityProject/Assets/BulletUnity/Scripts/Debug/BDebug.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace BulletSharp.Debug {
+    public class BDebug : MonoBehaviour
+    {
+        [Flags]
+        enum DebugType
+        {
+            None, Log, Warning, Error
+        }
+
+        static DebugType debugType;
+
+        public static void Log(object message)
+        {
+            UnityEngine.Debug.Log(message);
+        }
+
+        public static void LogWarning(object message)
+        {
+            UnityEngine.Debug.LogWarning(message);
+        }
+
+        public static void LogError(object message)
+        {
+            UnityEngine.Debug.LogError(message);
+        }
+    }
+}
