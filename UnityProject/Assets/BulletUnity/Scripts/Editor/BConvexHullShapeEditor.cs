bed55514b1f6357c301498681649b44f6792cd19
Jai <neo5icekcore@gmail.com>
2016-03-05 12:32:51 +0100

    Added editors for ConvexShape and TriangleShape

---
+++ UnityProject/Assets/BulletUnity/Scripts/Editor/BConvexHullShapeEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using BulletUnity;
+
+[CustomEditor(typeof(BConvexHullShape))]
+public class BConvexHullShapeEditor : Editor {
+
+    BConvexHullShape script;
+    SerializedProperty hullMesh;
+
+    void OnEnable()
+    {
+        script = (BConvexHullShape)target;
+        GetSerializedProperties();
+    }
+
+    void GetSerializedProperties()
+    {
+        hullMesh = serializedObject.FindProperty("hullMesh");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        if (script.transform.localScale != Vector3.one)
+        {
+            EditorGUILayout.HelpBox("This shape doesn't support scale of the object.\nThe scale must be one", MessageType.Warning);
+        }
+        EditorGUILayout.PropertyField(hullMesh);
+        serializedObject.ApplyModifiedProperties();
+    }
+}
