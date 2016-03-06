6a9d3ba46a45ef5e35624be13a5c046f24640d58
Phong13 <ian.deane@gmail.com>
2016-03-05 18:51:45 -0800

    Changes to the TerrainHeightfieldShape. It is almost working now.

---
+++ UnityProject/Assets/BulletUnity/Scripts/BTerrainCollisionObject.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using BulletSharp;
+
+namespace BulletUnity
+{
+    /* 
+    Custom verson of the collision object for handling heightfields to deal with some issues matching terrains to heighfields
+    1) Unity heitfiels have pivot at corner. Bullet heightfields have pivot at center
+    2) Can't rotate unity heightfields        
+    */
+    public class BTerrainCollisionObject : BCollisionObject
+    {
+        TerrainData td;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            Terrain t = GetComponent<Terrain>();
+            if (t != null)
+            {
+                td = t.terrainData;
+            }
+        }
+
+        //called by Physics World just before rigid body is added to world.
+        //the current rigid body properties are used to rebuild the rigid body.
+        internal override bool _BuildCollisionObject()
+        {
+            if (td == null)
+            {
+                Debug.LogError("Must be attached to an object with a terrain ");
+                return false;
+            }
+            BPhysicsWorld world = BPhysicsWorld.Get();
+            if (m_collisionObject != null)
+            {
+                if (isInWorld && world != null)
+                {
+                    isInWorld = false;
+                    world.RemoveCollisionObject(m_collisionObject);
+                }
+            }
+
+            if (transform.localScale != UnityEngine.Vector3.one)
+            {
+                Debug.LogError("The local scale on this collision shape is not one. Bullet physics does not support scaling on a rigid body world transform. Instead alter the dimensions of the CollisionShape.");
+            }
+
+            m_collisionShape = GetComponent<BCollisionShape>();
+            if (m_collisionShape == null)
+            {
+                Debug.LogError("There was no collision shape component attached to this BRigidBody. " + name);
+                return false;
+            }
+            if (!(m_collisionShape is BHeightfieldTerrainShape))
+            {
+                Debug.LogError("The collision shape needs to be a BHeightfieldTerrainShape. " + name);
+                return false;
+            }
+
+            CollisionShape cs = m_collisionShape.GetCollisionShape();
+            //rigidbody is dynamic if and only if mass is non zero, otherwise static
+
+            float originHeight = td.size.y / 4f;
+            if (m_collisionObject == null)
+            {
+                m_collisionObject = new CollisionObject();
+                m_collisionObject.CollisionShape = cs;
+                m_collisionObject.UserObject = this;
+
+                BulletSharp.Math.Matrix worldTrans = BulletSharp.Math.Matrix.Identity;
+                Vector3 pos = transform.position + new Vector3(td.heightmapWidth * .5f, originHeight, td.heightmapHeight * .5f);
+                worldTrans.Origin = pos.ToBullet();
+                m_collisionObject.WorldTransform = worldTrans;
+                m_collisionObject.CollisionFlags = m_collisionFlags;
+            }
+            else {
+                m_collisionObject.CollisionShape = cs;
+                BulletSharp.Math.Matrix worldTrans = BulletSharp.Math.Matrix.Identity;
+                Vector3 pos = transform.position + new Vector3(td.heightmapWidth * .5f, originHeight, td.heightmapHeight * .5f);
+                worldTrans.Origin = pos.ToBullet();
+                m_collisionObject.WorldTransform = worldTrans;
+                m_collisionObject.CollisionFlags = m_collisionFlags;
+            }
+            return true;
+        }
+
+        public override void SetPositionAndRotation(Vector3 position, Quaternion rotation)
+        {
+            if (isInWorld)
+            {
+                BulletSharp.Math.Matrix newTrans = m_collisionObject.WorldTransform;
+                newTrans.Origin = transform.position.ToBullet();
+                m_collisionObject.WorldTransform = newTrans;
+                transform.position = position;
+                transform.rotation = rotation;
+            } else
+            {
+                transform.position = position;
+                transform.rotation = rotation;
+            }
+        }
+
+    }
+}
--- UnityProject/Assets/BulletUnity/Scripts/CollisionShapes/BHeightfieldTerrainShape.cs
+++ UnityProject/Assets/BulletUnity/Scripts/CollisionShapes/BHeightfieldTerrainShape.cs
@@ -6,14 +6,19 @@ using System.Runtime.InteropServices;
 
 namespace BulletUnity {
     public class BHeightfieldTerrainShape : BCollisionShape {
-        public int width;
-        public int length;
-        public float heightScale;
-        public float maxHeight;
+
         public int upIndex;
-        public GCHandle pinnedTerrainData;
+        GCHandle pinnedTerrainData;
         PhyScalarType scalarType = PhyScalarType.Float;
 
+        public void Awake()
+        {
+            Terrain t = GetComponent<Terrain>();
+            if (t == null)
+            {
+                Debug.LogError("BHeightfieldTerrainShape must be attached to an object with a terrain." + name);
+            }
+        }
 
         public override void OnDrawGizmosSelected() {
 
@@ -21,20 +26,67 @@ namespace BulletUnity {
         
         public override CollisionShape GetCollisionShape() {
             if (collisionShapePtr == null) {
-                
+                Terrain t = GetComponent<Terrain>();
+                TerrainData td = t.terrainData;
+                int width = td.heightmapWidth;
+                int length = td.heightmapHeight;
+                float maxHeight = td.size.y / 2f;
+
                 //generate procedural data
-                byte[] terr = new byte[width * length * 4];
+                byte[] terr = new byte[width * length * sizeof(float)];
                 System.IO.MemoryStream file = new System.IO.MemoryStream(terr);
                 System.IO.BinaryWriter writer = new System.IO.BinaryWriter(file);
+
+                for (int i = 0; i < length; i++)
+                {
+                    float[,] row = td.GetHeights(0, i, width, 1);
+                    for (int j = 0; j < width; j++)
+                    {
+                        writer.Write((float)row[0, j] * 1000f);
+                    }
+                }
+
+                //2nd variation I tried flip x and y axis was wrong
+                /*
                 for (int i = 0; i < width; i++)
+                {
+                    float[,] col = td.GetHeights(i, 0, 1, length);
                     for (int j = 0; j < length; j++)
-                        writer.Write((float)((maxHeight / 2) + 4 * Math.Sin(j * 0.5f) * Math.Cos(i)));
+                    {
+                        writer.Write((float) col[j,0] * 1000f);
+                    }
+                }
+                */
+
+
+                /*
+                for (int i = length - 1; i >= 0; i--)
+                {
+                    float[,] row = td.GetHeights(0, i, width, 1);
+                    for (int j = length - 1; j >= 0; j--)
+                    {
+                        writer.Write((float)row[0, j] * 1000f);
+                    }
+                }
+                
+                for (int i = 0; i < width; i++)
+                {
+                    for (int j = 0; j < length; j++)
+                    {
+                        writer.Write((float)(i));
+                    }
+                }
+                */
+
                 writer.Flush();
                 file.Position = 0;
                 
                 pinnedTerrainData = GCHandle.Alloc(terr,GCHandleType.Pinned);
-                collisionShapePtr = new HeightfieldTerrainShape(128, 128, pinnedTerrainData.AddrOfPinnedObject(), heightScale, 0, maxHeight, upIndex, scalarType, false);
+
+                collisionShapePtr = new HeightfieldTerrainShape(width, length, pinnedTerrainData.AddrOfPinnedObject(), 1f, 0f, maxHeight, upIndex, scalarType, false);
                 ((HeightfieldTerrainShape)collisionShapePtr).SetUseDiamondSubdivision(true);
+                //just allocated several hundred float arrays. Garbage collect now since 99% likely we just loaded the scene
+                GC.Collect();
             }
             return collisionShapePtr;
         }
