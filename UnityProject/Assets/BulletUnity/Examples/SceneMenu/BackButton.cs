724a1cb661a2369b3cc8ca2cbad59cc86ae8660e
Phong13 <ian.deane@gmail.com>
2016-07-09 13:08:59 -0700

    Fixed the navigation between scenes so I can test examples on builds.

--- UnityProject/Assets/BulletUnity/Examples/SceneMenu/BackButton.cs
+++ UnityProject/Assets/BulletUnity/Examples/SceneMenu/BackButton.cs
@@ -4,17 +4,15 @@ using UnityEngine.SceneManagement;
 public class BackButton : MonoBehaviour {
 
 	public void OnGUI () {
-        UnityEngine.SceneManagement.Scene sc = SceneManager.GetSceneByName("ExampleMenu");
-        if (sc.IsValid())
+        GUI.BeginGroup(new Rect(Screen.width - 150, Screen.height - 40, 300, 400));
+        GUILayout.BeginVertical("box", GUILayout.Width(150));
+
+        if (GUILayout.Button("Back"))
         {
-            GUI.BeginGroup(new Rect(Screen.width - 150, Screen.height - 40, 300, 400));
-            GUILayout.BeginVertical("box", GUILayout.Width(150));
-
-            if (GUILayout.Button("Back"))
-                SceneManager.LoadScene(0);
-
-            GUILayout.EndVertical();
-            GUI.EndGroup();
+            SceneManager.LoadScene("ExampleMenu");
         }
-	}
+
+        GUILayout.EndVertical();
+        GUI.EndGroup();
+    }
 }
