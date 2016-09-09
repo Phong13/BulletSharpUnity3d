ea26e779fd02789d43125358140f8cc97b063232
Bamfax <Bamfax@users.noreply.github.com>
2016-09-09 11:46:43 +0200

    Usage Text and cleaned up.

--- UnityProject/Assets/BulletUnity/Scripts/BCamera.cs
+++ UnityProject/Assets/BulletUnity/Scripts/BCamera.cs
@@ -1,5 +1,4 @@
-﻿using UnityEngine;
-using System.Collections;
+using UnityEngine;
 using BulletUnity;
 using BulletSharp;
 
@@ -8,7 +7,15 @@ public class BCamera {
     private static CollisionObject closestRayResultReturnObj;
 
     public static CollisionObject ScreenPointToRay (Camera cam, Vector3 inputScreenCoords, CollisionFilterGroups rayFilterGroup, CollisionFilterGroups rayFilterMask) {
-        closestRayResultReturnObj = null;
+        /* Returns the first CollisionObject the ray hits.
+         * Requires as Input: 
+         * - Camera cam, UnityCamera from which the ray should be cast, e.g. Camera.main
+         * - Vector3 inputScreenCoords, the screenpoint through which the ray should be cast. E.g. mousepointer Input.mousePosition
+         * - CollisionFilterGroups rayFilterGroup, of which FilterGroup the ray belongs to
+         * - CollisionFilterGroups rayFilterMask, which FilterMask the ray has (to map to other Objects FilterGroups
+         * Be aware the Bulletphysics probably needs Group/Mask to match in both ways, i.e. Ray needs to collide with otherObj, as otherObj needs to collide with Ray.
+         * To get all Ray hits, see commented out Callback AllHitsRayResultCallback below.
+         * First version. Adapt to your needs. Refer to Bulletphysics.org for info. Make Pull Request to Phong BulletUnity with improvements.*/
 
         CollisionWorld collisionWorld = BPhysicsWorld.Get().world;
         BulletSharp.Math.Vector3 rayFrom = cam.transform.position.ToBullet();
@@ -21,18 +28,15 @@ public class BCamera {
 
         //Debug.Log("Casting ray from: " + rayFrom + " to: " + rayTo);
         collisionWorld.RayTest(rayFrom, rayTo, rayCallBack);
+
+        closestRayResultReturnObj = null;
         if (rayCallBack.HasHit)
         {
             //Debug.Log("rayCallBack " + rayCallBack.GetType() + " had a hit: " + rayCallBack.CollisionObject.UserObject + " / of type: " + rayCallBack.CollisionObject.UserObject.GetType());
             closestRayResultReturnObj = rayCallBack.CollisionObject;
-            rayCallBack.Dispose();
-            return closestRayResultReturnObj;
         }
-        else
-        {
-            //Debug.Log("rayCallBack " + rayCallBack.GetType() + " had no hits.");
-            rayCallBack.Dispose();
-            return closestRayResultReturnObj;
-        }
+
+        rayCallBack.Dispose();
+        return closestRayResultReturnObj;
     }
 }
