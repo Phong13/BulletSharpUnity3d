984251056e4399101e833eee8249c3a581440ced
Florian Nouviale <florian.nouviale@inria.fr>
2019-04-30 13:51:13 +0200

    Adding Threading capabilities to bullet

---
+++ Examples/Scenes/CollisionCallbacks/CollisionNormalDisplayCallback.cs
@@ -0,0 +1,96 @@
+﻿using BulletSharp;
+using BulletUnity;
+using UnityEngine;
+
+public class CollisionNormalDisplayCallback : BCollisionCallbacksDefault
+{
+
+    public Color CollisionStartColor = Color.red;
+    public Color CollisionStayColor = Color.red;
+
+    private Vector3 collisionPoint;
+    private Vector3 collistionNormal;
+    private bool MustDisplay = false;
+    private bool CollisionEnter = false;
+
+    /// <summary>
+    /// Lock object between threads
+    /// </summary>
+    System.Object lck = new object();
+
+    void Update()
+    {
+        CollisionToDisplay();
+    }
+
+    /// <summary>
+    /// This is called in Unity's thread
+    /// </summary>
+    private void CollisionToDisplay()
+    {
+        lock (lck)
+        {
+            if (MustDisplay)
+                Debug.DrawLine(collisionPoint, collisionPoint + collistionNormal, CollisionEnter ? CollisionStartColor : CollisionStayColor, 0.5f);
+            MustDisplay = false;
+            CollisionEnter = false;
+        }
+    }
+
+    /// <summary>
+    /// This is called in bullet thread
+    /// </summary>
+    /// <param name="other"></param>
+    /// <param name="manifoldList"></param>
+    public override void BOnCollisionEnter(CollisionObject other, PersistentManifoldList manifoldList)
+    {
+        foreach (PersistentManifold manifold in manifoldList.manifolds)
+        {
+            for (int i = 0; i < manifold.NumContacts; i++)
+            {
+                ManifoldPoint manifoldPoint = manifold.GetContactPoint(i);
+                lock (lck)
+                {
+                    collisionPoint = manifoldPoint.PositionWorldOnB.ToUnity();
+                    collistionNormal = manifoldPoint.NormalWorldOnB.ToUnity();
+                    MustDisplay = true;
+                    CollisionEnter = true;
+                }
+
+            }
+        }
+    }
+
+    /// <summary>
+    /// This is called in bullet thread
+    /// </summary>
+    /// <param name="other"></param>
+    /// <param name="manifoldList"></param>
+    public override void BOnCollisionStay(CollisionObject other, PersistentManifoldList manifoldList)
+    {
+        if (!CollisionEnter)
+        {
+            foreach (PersistentManifold manifold in manifoldList.manifolds)
+            {
+                for (int i = 0; i < manifold.NumContacts; i++)
+                {
+                    ManifoldPoint manifoldPoint = manifold.GetContactPoint(i);
+                    lock (lck)
+                    {
+                        collisionPoint = manifoldPoint.PositionWorldOnB.ToUnity();
+                        collistionNormal = manifoldPoint.NormalWorldOnB.ToUnity() / 10f;
+                        MustDisplay = true;
+                    }
+
+                }
+            }
+        }
+    }
+
+    public override void OnFinishedVisitingManifolds()
+    {
+        base.OnFinishedVisitingManifolds();
+        //it is safe to Instantiate, Destroy, Enable and Disable here
+    }
+}
+
---
+++ Scripts/BasePhysicsWorldHelper.cs
@@ -0,0 +1,43 @@
+﻿using BulletSharp;
+using UnityEngine;
+
+namespace BulletUnity
+{
+    public abstract class BasePhysicsWorldHelper : MonoBehaviour
+    {
+
+        internal int m__frameCount = 0;
+
+        internal BPhysicsWorld m_physicsWorld;
+        internal BDefaultCollisionHandler m_collisionEventHandler = new BDefaultCollisionHandler();
+        public void RegisterCollisionCallbackListener(BCollisionObject.BICollisionCallbackEventHandler toBeAdded)
+        {
+            if (m_collisionEventHandler != null) m_collisionEventHandler.RegisterCollisionCallbackListener(toBeAdded);
+        }
+
+        public void DeregisterCollisionCallbackListener(BCollisionObject.BICollisionCallbackEventHandler toBeRemoved)
+        {
+            if (m_collisionEventHandler != null) m_collisionEventHandler.DeregisterCollisionCallbackListener(toBeRemoved);
+        }
+
+        internal DiscreteDynamicsWorld m_ddWorld;
+        internal CollisionWorld m_world;
+
+        public float FixedTimeStep
+        {
+            get
+            {
+                return m_physicsWorld.fixedTimeStep;
+            }
+        }
+
+        public float TimeStepRatio
+        {
+            get
+            {
+                return m_physicsWorld.timeStepRatio;
+            }
+        }
+
+    }
+}
