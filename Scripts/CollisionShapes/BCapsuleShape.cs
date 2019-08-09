9acf43e86d02204fcbeb92f80a7af52da0b9e65d
raisinrand <thebirdbag@gmail.com>
2019-08-08 20:23:57 -0400

    Revert "made capsule gizmo work with localscaling"

--- Scripts/CollisionShapes/BCapsuleShape.cs
+++ Scripts/CollisionShapes/BCapsuleShape.cs
@@ -87,9 +87,8 @@ namespace BulletUnity
             {
                 rotation = Quaternion.AngleAxis(90, transform.right) * rotation;
             }
-            float scaledRadius = radius * LocalScaling[(((int)upAxis) + 2) % 3];
-            float scaledHeight = height * LocalScaling[(int)upAxis];
-            BUtility.DebugDrawCapsule(position, rotation, Vector3.one, scaledRadius, scaledHeight / 2f, 1, Gizmos.color);
+            BUtility.DebugDrawCapsule(position, rotation, LocalScaling, radius, height / 2f, 1, Gizmos.color);
+
         }
 
         CapsuleShape _CreateCapsuleShape()
